e8321a500d4b47235ab920b41994ab7f555cece8
Saleh Ahmad <105820062+saleh5834@users.noreply.github.com>
2023-05-20 10:17:27 +0600

    Add files via upload

---
+++ FoodOrder/src/TestFoodOrder.java
@@ -0,0 +1,13 @@
+
+public class TestFoodOrder {
+
+    
+    public static void main(String[] args) {
+        FoodOrder fo = new FoodOrder();
+
+        fo.displayMenu();
+
+        fo.order();
+    }
+    
+}
